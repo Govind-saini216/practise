b7e24ce86aa4ee30ca4d44287853d6ea57c82496
Govind <Govindsain7737@gmail.com>
2023-12-01 14:45:40 +0530

    changes it now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,13 +37,14 @@ const Navbar = ({setData,cart}) => {
             // onClick={handleSubmit} 
             onSubmit={handleSubmit}
              className="search-bar">
-                <input 
+                {/* <input 
                 value={searchTerm}
                 onChange={(e)=>setSearchTerm(e.target.value)}
                 type="text"
                  placeholder='Search Products'
-                  />
+                  /> */}
 
+                  <input type="color"/>
             </form>
 
             <Link to={'/cart'} className="cart">
